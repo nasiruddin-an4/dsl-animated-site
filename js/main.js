45f9093787e0c65760ff9ffd49e23f70427f961f
Nasir Uddin <74647944+nasiruddin-an4@users.noreply.github.com>
2024-11-10 16:02:11 +0600

    new file

--- js/main.js
+++ js/main.js
@@ -96,27 +96,26 @@
 
 })(jQuery);
 
-// Counter Js
+    // Counter Js
+    (() => {
+        const counter = document.querySelectorAll('.counter');
+        // covert to array
+        const array = Array.from(counter);
+        // select array element
+        array.map((item) => {
+            // data layer
+            let counterInnerText = item.textContent;
 
-(() => {
-    const counter = document.querySelectorAll('.counter');
-    // covert to array
-    const array = Array.from(counter);
-    // select array element
-    array.map((item) => {
-        // data layer
-        let counterInnerText = item.textContent;
-
-        let count = 1;
-        let speed = item.dataset.speed / counterInnerText
-        function counterUp() {
-            item.textContent = count++
-            if (counterInnerText < count) {
-                clearInterval(stop);
+            let count = 1;
+            let speed = item.dataset.speed / counterInnerText
+            function counterUp() {
+                item.textContent = count++
+                if (counterInnerText < count) {
+                    clearInterval(stop);
+                }
             }
-        }
-        const stop = setInterval(() => {
-            counterUp();
-        }, speed);
-    })
-})()
+            const stop = setInterval(() => {
+                counterUp();
+            }, speed);
+        })
+    })()
